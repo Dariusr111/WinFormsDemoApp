80fa621656e41b744058c46060ed86acaa9bf8da
dariusr <darius.r111@gmail.com>
2023-01-25 15:47:41 +0200

    Select file

--- WinFormsDemo/Form1.Designer.cs
+++ WinFormsDemo/Form1.Designer.cs
@@ -28,12 +28,57 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(318, 321);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(176, 68);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Importuoti į DB";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(217, 96);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(222, 23);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(481, 96);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(137, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Pasirinkite failą";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private Button button1;
+		private TextBox textBox1;
+		private Button button2;
 	}
 }
--- WinFormsDemo/Form1.cs
+++ WinFormsDemo/Form1.cs
@@ -6,5 +6,31 @@ namespace WinFormsDemo
 		{
 			InitializeComponent();
 		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Title = "Select File";
+			openFileDialog.InitialDirectory = @"C:\";
+			openFileDialog.Filter = "All files (*.*)|*.*|Excel File (*.csv)|*.csv";
+			openFileDialog.FilterIndex = 2;
+			openFileDialog.ShowDialog();
+			if (openFileDialog.FileName != "")
+			{ textBox1.Text = openFileDialog.FileName; }
+			else
+			{ textBox1.Text = "You didn't select the file!"; }
+
+		}
 	}
 }
