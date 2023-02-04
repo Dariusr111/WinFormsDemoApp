2b1654f31537f869f01e27634f8610eea9912858
dariusr <darius.r111@gmail.com>
2023-02-04 12:06:36 +0200

    Components import finished

--- WinFormsDemo/Form1.cs
+++ WinFormsDemo/Form1.cs
@@ -170,6 +170,99 @@ namespace WinFormsDemo
 			else if (radioButton3.Checked == true)
 			{
 				label3.Hide();
+				if (path1 != null)
+				{
+					// READING CSV
+					using var streamReader = File.OpenText(path1);  // Path to Cvs file
+					using var csvReader = new CsvReader(streamReader, CultureInfo.CurrentCulture);
+
+
+					// CHECKING IF TABLE EXIST
+					Database databaseObject = new Database();
+					string query = "SELECT name FROM sqlite_master WHERE type='table' AND name='components'";
+					SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+					databaseObject.OpenConnection();
+					SQLiteDataReader result = myCommand.ExecuteReader();
+
+
+					// if table !exist -> creating new
+					if (!result.HasRows)
+					{
+						// CREATE DATABASE		
+						string query1 = "CREATE TABLE components (`id` INTEGER, `in1` TEXT, `in2` TEXT, `in3` TEXT, `in4` TEXT, `in5` TEXT, `quantity` INTEGER, `description` TEXT, `material1` TEXT, `material2` TEXT, `material3` TEXT, `material4` TEXT, PRIMARY KEY(`id` AUTOINCREMENT));";
+
+						//Database databaseObject = new Database();
+						SQLiteCommand myCommand1 = new SQLiteCommand(query1, databaseObject.myConnection);
+						//databaseObject.OpenConnection();
+						myCommand1.CommandText = query1;
+						myCommand1.ExecuteNonQuery();
+
+						// INSERT INTO DATABASE		
+						string query2 = "INSERT INTO components (`in1`, `in2`, `in3`, `in4`, `in5`, `quantity`, `description`, `material1`, `material2`, `material3`, `material4`) VALUES (@v0, @v1, @v2, @v3, @v4, @v5, @v6, @v7, @v8, @v9, @v10);";
+
+						SQLiteCommand myCommand2 = new SQLiteCommand(query2, databaseObject.myConnection);
+
+						string value;
+
+						while (csvReader.Read())
+						{
+							for (int i = 0; csvReader.TryGetField<string>(i, out value); i++)
+							{
+
+								myCommand2.Parameters.AddWithValue("@v" + i, value);
+								Console.WriteLine(value);
+
+							}
+
+							myCommand2.ExecuteNonQuery();
+
+						}
+
+						databaseObject.CloseConection();
+						Console.ReadLine();
+
+					}
+
+					else
+					{
+						// INSERT INTO DATABASE		
+
+						string query2 = "INSERT INTO components (`in1`, `in2`, `in3`, `in4`, `in5`, `quantity`, `description`, `material1`, `material2`, `material3`, `material4`) VALUES (@v0, @v1, @v2, @v3, @v4, @v5, @v6, @v7, @v8, @v9, @v10);";
+
+						//Database databaseObject = new Database();
+						SQLiteCommand myCommand2 = new SQLiteCommand(query2, databaseObject.myConnection);
+						databaseObject.OpenConnection();
+
+						string value;
+
+						while (csvReader.Read())
+						{
+							for (int i = 0; csvReader.TryGetField<string>(i, out value); i++)
+							{
+
+								myCommand2.Parameters.AddWithValue("@v" + i, value);
+								Console.WriteLine(value);
+
+							}
+
+							myCommand2.ExecuteNonQuery();
+
+						}
+
+						databaseObject.CloseConection();
+						Console.ReadLine();
+
+
+					}
+
+					label1.Show();
+
+				}
+
+				else
+				{
+					textBox1.Text = "Jûs nepasirinkote failo!";
+				}
 
 			}
 
